83a59ad7858abd6c412ce5e9262e8bf03d6617e7
Owais Abbas <ozabbas95@gmail.com>
2021-09-04 16:45:22 -0400

    wk3

---
+++ WK2_2.rb
@@ -0,0 +1,58 @@
+# 1. Write a while loop to print the numbers 1 through 10.
+
+num = 1
+while num <= 10
+  p num
+  num += 1
+end
+# 2. Write a while loop that prints the word "hello" 5 times.
+
+count = 0
+while count < 5
+  p "hello"
+  count += 1
+end
+
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+while true
+  p "Enter a word: "
+  word = gets.chomp
+  if word == "stop"
+    break
+  else
+    p word
+  end
+end
+
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+number = 0
+while number <= 100
+  p number
+  number += 5
+end
+
+# 5. Write a while loop that prints the number 9000 ten times.
+
+numb = 9000
+count = 0
+while count < 10
+  p numb
+  count += 1
+end
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+numb2 = 
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/c4ec0e0f8f6e123d65ada9bf3100068b
