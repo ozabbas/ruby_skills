2181b08d812264cb2adb478cef6b32893fd533b3
Owais Abbas <ozabbas95@gmail.com>
2021-10-15 22:26:32 -0500

    new code added to array practice

--- WK3_1.rb
+++ WK3_1.rb
@@ -43,6 +43,10 @@ end
 
 # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
 
+array7 = ['hey', 'hello']
+array7 << 'hi there'
+p array7
+
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
