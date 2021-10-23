ab351ca2dc97399ba40d3f464b39fa97ce70bdb0
Owais Abbas <ozabbas95@gmail.com>
2021-10-22 19:41:37 -0500

    ruby practice

--- WK3_2.rb
+++ WK3_2.rb
@@ -1,5 +1,10 @@
 # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
 
+person = { "first_name" => "Ada", "last_name" => "Lovelace", "email" => "ada.lovelace@email.com" }
+p person["first_name"]
+p person["last_name"]
+p person["email"]
+
 # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
 
 # 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
